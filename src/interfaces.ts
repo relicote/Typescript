9b04cdf64abfbc93504885931ffa006c9c21c91c
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-26 12:03:31 -0300

    att

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,6 @@
 // Interfaces (type x interface)
 
 
-interface robot2{
-    readonly id: number | string;
-    name: string;
-}
-
-const bot2: robot2 ={
-    id:1,
-    name: 'Robot',
-};
 
 type robot = {
     id: number | string;
@@ -21,5 +12,32 @@ const bot: robot ={
     name: 'Megabosta',
 };
 
-console.log(bot);
-console.log(bot2);
+interface robot2{
+    readonly id: number | string;
+    name: string;
+    sayHello(): string;
+}
+
+const bot2: robot2 ={
+    id: 1,
+    name: 'Robot',
+    sayHello: function (): string {
+        throw new Error("Function not implemented.");
+    }
+};
+
+class pessoa implements robot2{
+    id: string | number;
+    name: string;
+
+    constructor(id: string | number, name: string){
+        this.id = id
+        this.name = name
+    }
+    sayHello(): string {
+        return `Hello ${this.name}`;
+    }
+}
+
+const p = new pessoa(1, 'Gabrielzito')
+console.log(p.sayHello());
