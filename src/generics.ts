ba09d8c123d6d96f6e0a0ebe722999555d0e6ca0
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-26 12:43:19 -0300

    att

--- src/generics.ts
+++ src/generics.ts
@@ -6,7 +6,5 @@ function concatArray<T>(...itens: T[]): T[]{
 const numArray = concatArray<number[]>([1,5],[3]);
 const stgArray = concatArray<string[]>(['Gabriel', 'Ronaldo'], ['Kaka']);
 
-numArray.push('Salve veinho');
-
 console.log(numArray);
 console.log(stgArray);
--- src/index.ts
+++ src/index.ts
@@ -51,3 +51,5 @@
 // Datas
     let aniversario:Date = new Date('2022-12-01 05:00')
     // console.log(aniversario.toString());
+
+    console.log(dados)
